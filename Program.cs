cf149175c8a7e9d38313f70dd767d932477acd55
Silvia-Du <silviadu@hotmail.it>
2022-09-15 15:53:12 +0200

    Completato bonus esercitazione

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
+//array dispari per il test
+//string[] favouriteFoods = { "pesche", "smoothie", "biscotti saraceno", "pizza", "sogliole", "humus" , "pasta al tonno"};
+string[] favouriteFoods = { "pesche", "smoothie", "biscotti saraceno", "pizza", "sogliole", "humus"};
 
-string[] favouriteFoods = { "pesche", "smoothie", "biscotti saraceno", "pizza", "sogliole", "humus" };
-
+int half = favouriteFoods.Length / 2;
 //lunghezza dell'array
 Console.WriteLine(favouriteFoods.Length);
 
@@ -23,3 +27,16 @@ Console.WriteLine(favouriteFoods[favouriteFoods.Length -1]);
 
 //bonus
 
+if (favouriteFoods.Length % 2 == 0)
+{
+    Console.WriteLine("l\'array lenght è pari");
+    Console.WriteLine(favouriteFoods[half-1]);
+    Console.WriteLine(favouriteFoods[half]);
+
+}
+
+else
+{
+    Console.WriteLine("l'array lenght è dispari");
+    Console.WriteLine(favouriteFoods[(favouriteFoods.Length -1)/2]);
+}
